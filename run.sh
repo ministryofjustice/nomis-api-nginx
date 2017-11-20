0c61a1fd07b4b4036cfe8731731a4afa0b2e64c1
Michael Willis <michael.willis@syscon.net>
2017-11-20 16:35:03 +0000

    NGINX - APP1 & APP2 Config

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop elite2-api
-docker rm -vf elite2-api
-docker pull sysconjusticesystems/elite2-api
-docker run -d --name elite2-api -h elite2-api --restart=always  -e SERVER_PORT=8080 -e SPRING_PROFILES_ACTIVE=dev,nomis -e SPRING_DATASOURCE_URL=jdbc:oracle:thin:@lonlx01:1521:pvb_dev sysconjusticesystems/elite2-api
-exit 0
